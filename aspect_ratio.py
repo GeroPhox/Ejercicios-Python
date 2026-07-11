ec2fe2081b1c60bc360351e149bed81307d180f1
GeroPhox <Gerophox@gmail.com>
2026-07-11 19:14:59 +0200

    Imagen en bytes

--- aspect_ratio.py
+++ aspect_ratio.py
@@ -3,11 +3,18 @@
 # url de ejemplo: https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png
 import math
 import requests
+from PIL import Image 
+import io
+
 
 #url = input("Inserte URL")
 url = "https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png"
-url = requests.get(url)
-print(url.content)
+response = requests.get(url)
+undefined_image = response.content
+image = io.BytesIO(undefined_image)
+print(open(image))
 
-def calculate_aspect_ratio(url, height, width):
+
+
+def calculate_aspect_ratio(response, height, width):
     math.gcd(height, width)
