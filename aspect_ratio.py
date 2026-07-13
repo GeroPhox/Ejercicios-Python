c6cc0b04ee973462fc5e3489295921580afc7782
GeroPhox <Gerophox@gmail.com>
2026-07-13 14:11:50 +0200

    Version final

--- aspect_ratio.py
+++ aspect_ratio.py
@@ -7,14 +7,35 @@ from PIL import Image
 import io
 
 
-#url = input("Inserte URL")
-url = "https://raw.githubusercontent.com/mouredev/mouredev/master/mouredev_github_profile.png"
+url = input("Inserte URL")
+
+
+# Ve a esa dirección y traeme lo que hay alli
 response = requests.get(url)
-undefined_image = response.content
-image = io.BytesIO(undefined_image)
-print(open(image))
 
+# Sacar el contenido del url
+bytes_image = response.content
 
+# Crear un archivo que solo existe cuando se printea ya que Pillow solo abre archivos
+tidied_image_bytes = io.BytesIO(bytes_image)
 
-def calculate_aspect_ratio(response, height, width):
-    math.gcd(height, width)
+# Abrir el archivo
+image = Image.open(tidied_image_bytes)
+
+# Desempaquetar altura y ancho de la imagen
+size = image.size
+width, height = size
+
+# Otra manera de desempaquetar la altura y el ancho de la imagen
+width = size[0]
+height = size[1]
+
+# Variable que nos da el minimo comun multiplo
+mcd = math.gcd(width, height)
+
+# Funcion para obtener el aspect ratio
+def calculate_aspect_ratio(width, height):
+    print(width / mcd,":", height / mcd)
+
+# Print del aspect ratio
+calculate_aspect_ratio(width, height)
